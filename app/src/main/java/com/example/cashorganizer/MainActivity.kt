7776ecf586f4c363efea2189aadec89b28a94330
Kittithee Thaweesuk <zkittithee.t@pttdigital.com>
2024-01-05 01:39:00 +0700

    feat: add custom toolbar fix: change status bar color

--- app/src/main/java/com/example/cashorganizer/MainActivity.kt
+++ app/src/main/java/com/example/cashorganizer/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.cashorganizer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.cashorganizer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        initUI()
+    }
+
+    private fun initUI() {
+        binding.customToolbar.ivAccount.setOnClickListener{
+            Toast.makeText(this, "Account clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
