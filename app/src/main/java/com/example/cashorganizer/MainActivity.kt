4b1da74859a754b6df0701dd377a780477871f4e
Kittithee Thaweesuk <zkittithee.t@pttdigital.com>
2024-01-14 00:03:57 +0700

    fix: add icon header menu income statement, staement and account feat: add card select date period v1

--- app/src/main/java/com/example/cashorganizer/MainActivity.kt
+++ app/src/main/java/com/example/cashorganizer/MainActivity.kt
@@ -1,18 +1,66 @@
 package com.example.cashorganizer
 
+import android.app.DatePickerDialog
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.DatePicker
+import android.widget.TextView
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
 import com.example.cashorganizer.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
+    private lateinit var txtPeriod: TextView
+    private lateinit var btnDatePicker: CardView
+    private val calendar = Calendar.getInstance()
+    private val fullMonth: List<String> = listOf("มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน", "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // Initialize views
+        txtPeriod = findViewById(R.id.txtPeriod)
+        btnDatePicker = findViewById(R.id.cardPeriod)
         initUI()
+
+        // Action click
+        btnDatePicker.setOnClickListener {
+            // Show the DatePicker dialog
+            showDatePicker()
+        }
+    }
+
+    private fun showDatePicker() {
+        // Create a DatePickerDialog
+        val datePickerDialog = DatePickerDialog(
+            this, {DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int ->
+                // Create a new Calendar instance to hold the selected date
+                val selectedDate = Calendar.getInstance()
+                // Set the selected date using the values received from the DatePicker dialog
+                selectedDate.set(year, monthOfYear, dayOfMonth)
+                // Create a SimpleDateFormat to format the date as "dd/MM/yyyy"
+                val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                // Format the selected date into a string
+                val formattedDate = dateFormat.format(selectedDate.time)
+                // Update the TextView to display the selected date with the "Selected Date: " prefix
+                val monthName = fullMonth[monthOfYear]
+                val yearThai = year + 543
+                txtPeriod.text = "รอบวันที่ $dayOfMonth $monthName $yearThai"
+                  },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        // Show the DatePicker dialog
+        datePickerDialog.show()
     }
 
     private fun initUI() {
